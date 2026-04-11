1d236d486f747b461009f046c32cd6834755928e
Andrey <dorofeev_andrey03@bk.ru>
2026-04-11 18:05:57 +0500

    feat: add base model and session to db

---
+++ src/db/session.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from src.core.config import get_settings
+
+settings = get_settings()
+
+engine = create_async_engine(
+    settings.postgres_connection_url_async,
+    echo=True,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_db_session():
+    async with AsyncSessionLocal() as session:
+        yield session
