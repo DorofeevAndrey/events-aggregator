a3d22206b89e7ed204d4f39d875a5b5e1b7b697d
Andrey <dorofeev_andrey03@bk.ru>
2026-04-13 22:54:46 +0500

    fix: pytest piece of github action

--- run.sh
+++ run.sh
@@ -5,8 +5,5 @@ set -e
 echo "Running migrations..."
 alembic upgrade head
 
-echo "Running provider tests..."
-pytest -v
-
 echo "Starting API..."
 exec uvicorn src.main:app --host 0.0.0.0 --port 8000
