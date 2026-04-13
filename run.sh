9b730fca83f7817f86d1a2cef3c424a6794a2b62
Andrey <dorofeev_andrey03@bk.ru>
2026-04-13 22:12:17 +0500

    feat: add test on start app

--- run.sh
+++ run.sh
@@ -5,5 +5,8 @@ set -e
 echo "Running migrations..."
 alembic upgrade head
 
+echo "Running provider tests..."
+pytest -v
+
 echo "Starting API..."
 exec uvicorn src.main:app --host 0.0.0.0 --port 8000
