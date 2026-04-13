3fe544ee993d8f3e4d1c225339a964ff1bf35282
Andrey <dorofeev_andrey03@bk.ru>
2026-04-14 00:46:30 +0500

    feat: create func get seats from api and port for this

--- src/infrastructure/events_provider/client.py
+++ src/infrastructure/events_provider/client.py
@@ -44,3 +44,19 @@ class EventsProviderClient:
             raise
 
         return response.json()
+
+    async def seats(self, event_id: str) -> dict[str, Any]:
+        try:
+            response = await self._client.get(url=f"/api/events/{event_id}/seats/")
+            response.raise_for_status()
+        except httpx.HTTPStatusError as e:
+            logger.error(
+                "Events seats API returned error",
+                extra={
+                    "status_code": e.response.status_code,
+                    "event_id": event_id,
+                },
+            )
+            raise
+
+        return response.json()
