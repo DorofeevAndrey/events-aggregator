04724e8ad1f4a7b712da966f66165d7e750abb6e
Andrey <dorofeev_andrey03@bk.ru>
2026-04-13 14:55:22 +0500

    feat: create service/usecase for get events

---
+++ src/application/ports/events.py
@@ -0,0 +1,11 @@
+from datetime import date
+from typing import Protocol
+from src.db.models.events import Event
+
+
+class EventRepositoryPort(Protocol):
+    async def count(self, date_from: date | None) -> int: ...
+
+    async def list(
+        self, date_from: date | None = None, offset: int = 0, limit: int = 20
+    ) -> list[Event]: ...
---
+++ src/application/service/events.py
@@ -0,0 +1,46 @@
+from datetime import date
+
+from fastapi import Request
+
+from src.application.ports.events import EventRepositoryPort
+
+
+class EventsService:
+    def __init__(self, events_repository: EventRepositoryPort):
+        self._event_repository = events_repository
+
+    async def list_events(
+        self, request: Request, date_from: date | None, page: int, page_size: int
+    ):
+        offset = (page - 1) * page_size
+        count = self._event_repository.count(date_from=date_from)
+        events = self._event_repository.list(
+            date_from=date_from, offset=offset, limit=page_size
+        )
+
+        next_url = None
+        if offset + page_size < count:
+            next_url = str(
+                request.url.include_query_params(
+                    page=page + 1,
+                    page_size=page_size,
+                    date_from=date_from.isoformat() if date_from else None,
+                )
+            )
+
+        previous_url = None
+        if page > 1:
+            previous_url = str(
+                request.url.include_query_params(
+                    page=page - 1,
+                    page_size=page_size,
+                    date_from=date_from.isoformat() if date_from else None,
+                )
+            )
+
+        return {
+            "count": count,
+            "next": next_url,
+            "previous": previous_url,
+            "results": events,
+        }
