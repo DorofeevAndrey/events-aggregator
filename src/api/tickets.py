9ddc13e1fca7ccb51e21d6ec0e27ff273340e236
Andrey <dorofeev_andrey03@bk.ru>
2026-04-14 16:37:05 +0500

    feat: create route /api/tickets

---
+++ src/api/tickets.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.application.service.tickets import TicketsService
+from src.core.config import get_settings
+from src.db.session import get_db_session
+from src.infrastructure.events_provider.client import EventsProviderClient
+from src.repositories.event_repository import EventRepository
+from src.schemas.tickets.ticket_request import TicketRequestSchema
+from src.schemas.tickets.ticket_response import TicketResponseSchema
+
+router = APIRouter()
+
+
+@router.post("/tickets", response_model=TicketResponseSchema, status_code=201)
+async def create_ticket(
+    params: TicketRequestSchema, session: AsyncSession = Depends(get_db_session)
+):
+    settings = get_settings()
+
+    async with EventsProviderClient(
+        base_url=settings.events_provider_base_url,
+        api_key=settings.events_provider_api_key,
+    ) as client:
+        service = TicketsService(
+            client=client,
+            event_repository=EventRepository(session=session),
+        )
+        return await service.create_ticket(
+            event_id=params.event_id,
+            first_name=params.first_name,
+            last_name=params.last_name,
+            email=params.email,
+            seat=params.seat,
+        )
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, FastAPI
 from src.api.events import router as events_router
 from src.api.health import router as health_router
 from src.api.sync import router as sync_router
+from src.api.tickets import router as tickets_router
 from src.core import logging as logging_config
 from src.core.lifespan import lifespan
 
@@ -16,5 +17,6 @@ api_router.include_router(health_router)
 api_router.include_router(sync_router)
 
 api_router.include_router(events_router)
+api_router.include_router(tickets_router)
 
 app.include_router(api_router)
