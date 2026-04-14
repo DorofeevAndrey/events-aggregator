5d21d6bff8ea3af9fdd702df8fc66eba620e823e
Andrey <dorofeev_andrey03@bk.ru>
2026-04-14 17:06:51 +0500

    feat: add tickets table on alembic commit

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,7 @@ from src.db.base import Base
 from src.db.models.events import Event  # noqa: F401
 from src.db.models.places import Place  # noqa: F401
 from src.db.models.sync_state import SyncState  # noqa: F401
+from src.db.models.tickets import Ticket  # noqa: F401
 from src.db.session import settings
 
 # this is the Alembic Config object, which provides
